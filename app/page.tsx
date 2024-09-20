3c465076a8c0a5fa7af65d9a9b5562c410b604fc
Alex <alexperezdavies@gmail.com>
2024-09-20 16:31:51 +0100

    use world map on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import WorldMap from '../components/WorldMap';
 export default function Home() {
-  return (
-    <div>
-      <h1>Travel Blog</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Travel Blog</h1>
+            <WorldMap />
+        </div>
+    );
 }
