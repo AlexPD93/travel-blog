b0489bd9c611ac04e206215dadddb589368368ce
Alex Perez-Davies <alexperezdavies@gmail.com>
2024-11-01 12:21:01 +0000

    change title (#6)

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import WorldMap from "../components/WorldMap";
 export default function Home() {
   return (
     <div>
-      <h1>Travel Blogus</h1>
+      <h1>Travel Blogs</h1>
       <WorldMap />
     </div>
   );
